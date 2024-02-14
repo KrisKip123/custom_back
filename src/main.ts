1e63c5bf7aac580fcc30313aecf8d96e9f77d272
KrisKip123 <kriskip12393@gmail.com>
2024-02-14 13:24:48 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Example')
+    .setDescription('Example descript')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+
+  const documentSwagger = SwaggerModule.createDocument(app, configSwagger);
+
+  SwaggerModule.setup('api', app, documentSwagger);
+
   await app.listen(3000);
 }
 bootstrap();
